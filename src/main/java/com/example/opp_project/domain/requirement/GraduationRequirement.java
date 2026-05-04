32400fb8073df5e355ab943b40b5ccd634bf5a9e
taesungs <70329288+taesungs@users.noreply.github.com>
2026-05-04 19:29:18 +0900

    졸업요건 객체 방식 변경

--- src/main/java/com/example/opp_project/domain/requirement/GraduationRequirement.java
+++ src/main/java/com/example/opp_project/domain/requirement/GraduationRequirement.java
@@ -1,53 +1,109 @@
 package com.example.opp_project.domain.requirement;
 
-import com.example.opp_project.domain.department.Department;
-
 import java.util.List;
 
 public class GraduationRequirement {
 
-    private final Department department;
+    private final String departmentName;
+    private final int departmentCode;
+    private final int admissionYear;
 
-    private final int totalCredits;
-    private final int majorCredits;
-    private final int liberalCredits;
+    private final int requiredTotalCredits;
+    private final int requiredMajorCredits;
+    private final int requiredLiberalCredits;
+    private final int requiredEtcCredits;
+
+    private final boolean engineeringProgramRequired;
+    private final boolean majorRequiredSubjectsRequired;
+    private final boolean multiMajorRequired;
+
+    private final int requiredDesignCredits;
+    private final int requiredMscCredits;
 
     private final List<String> majorRequiredSubjects;
     private final List<String> engineeringRequiredSubjects;
     private final List<String> graduationRequiredItems;
 
     public GraduationRequirement(
-            Department department,
-            int totalCredits,
-            int majorCredits,
-            int liberalCredits,
+            String departmentName,
+            int departmentCode,
+            int admissionYear,
+            int requiredTotalCredits,
+            int requiredMajorCredits,
+            int requiredLiberalCredits,
+            int requiredEtcCredits,
+            boolean engineeringProgramRequired,
+            boolean majorRequiredSubjectsRequired,
+            boolean multiMajorRequired,
+            int requiredDesignCredits,
+            int requiredMscCredits,
             List<String> majorRequiredSubjects,
             List<String> engineeringRequiredSubjects,
             List<String> graduationRequiredItems
     ) {
-        this.department = department;
-        this.totalCredits = totalCredits;
-        this.majorCredits = majorCredits;
-        this.liberalCredits = liberalCredits;
+        this.departmentName = departmentName;
+        this.departmentCode = departmentCode;
+        this.admissionYear = admissionYear;
+        this.requiredTotalCredits = requiredTotalCredits;
+        this.requiredMajorCredits = requiredMajorCredits;
+        this.requiredLiberalCredits = requiredLiberalCredits;
+        this.requiredEtcCredits = requiredEtcCredits;
+        this.engineeringProgramRequired = engineeringProgramRequired;
+        this.majorRequiredSubjectsRequired = majorRequiredSubjectsRequired;
+        this.multiMajorRequired = multiMajorRequired;
+        this.requiredDesignCredits = requiredDesignCredits;
+        this.requiredMscCredits = requiredMscCredits;
         this.majorRequiredSubjects = majorRequiredSubjects;
         this.engineeringRequiredSubjects = engineeringRequiredSubjects;
         this.graduationRequiredItems = graduationRequiredItems;
     }
 
-    public Department getDepartment() {
-        return department;
+    public String getDepartmentName() {
+        return departmentName;
     }
 
-    public int getTotalCredits() {
-        return totalCredits;
+    public int getDepartmentCode() {
+        return departmentCode;
     }
 
-    public int getMajorCredits() {
-        return majorCredits;
+    public int getAdmissionYear() {
+        return admissionYear;
     }
 
-    public int getLiberalCredits() {
-        return liberalCredits;
+    public int getRequiredTotalCredits() {
+        return requiredTotalCredits;
+    }
+
+    public int getRequiredMajorCredits() {
+        return requiredMajorCredits;
+    }
+
+    public int getRequiredLiberalCredits() {
+        return requiredLiberalCredits;
+    }
+
+    public int getRequiredEtcCredits() {
+        return requiredEtcCredits;
+    }
+
+    public boolean isEngineeringProgramRequired() {
+        return engineeringProgramRequired;
+    }
+
+    public boolean isMajorRequiredSubjectsRequired() {
+        return majorRequiredSubjectsRequired;
+    }
+
+    public boolean isMultiMajorRequired() {
+        return multiMajorRequired;
+    }
+
+    public int getRequiredDesignCredits() {
+        return requiredDesignCredits;
+    }
+
+    public int getRequiredMscCredits() {
+        return requiredMscCredits;
     }
 
     public List<String> getMajorRequiredSubjects() {
